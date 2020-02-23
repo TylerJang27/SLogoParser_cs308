86340adb50305ec06e1c658817595a9c7c80e7cd
Shruthi Kumar <sk593@duke.edu>
2020-02-23 15:29:44 -0500

    renamed current branch from shruthi to FrontEnd001

--- src/slogo/frontendexternal/PenView.java
+++ src/slogo/frontendexternal/PenView.java
@@ -5,21 +5,36 @@ import javafx.beans.property.SimpleBooleanProperty;
 
 public class PenView {
   private SimpleBooleanProperty isDown;
-  private Color penColor;
-  private double lineThickness;
-  private double xPos;
-  private double yPos;
+  private Color myPenColor;
+  private double myLineThickness;
+  private double myXPos;
+  private double myYPos;
 
   public PenView() {
     isDown = new SimpleBooleanProperty(false);
-    penColor = Color.BLACK;
-    lineThickness = .5;
-    xPos = 0;
-    yPos = 0;
+    myPenColor = Color.BLACK;
+    myLineThickness = .5;
+    myXPos = 0;
+    myYPos = 0;
   }
 
   public void setPenDown() {
-
+    isDown.set(true);
   }
+
+
+  public boolean getPenDown() {
+    return isDown.get();
+  }
+
+  public double getMyLineThickness() {
+    return myLineThickness;
+  }
+
+  public void setMyLineThickness(double myLineThickness) {
+    this.myLineThickness = myLineThickness
+  }
+
+
 
 }

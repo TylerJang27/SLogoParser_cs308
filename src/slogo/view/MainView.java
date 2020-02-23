43cd6dfa1512958df044f572d74d0da5d37e8cda
Shruthi Kumar <sk593@duke.edu>
2020-02-22 19:07:04 -0500

    added text input

--- src/slogo/view/MainView.java
+++ src/slogo/view/MainView.java
@@ -4,7 +4,16 @@ import javafx.scene.layout.BorderPane;
 
 public class MainView extends BorderPane {
 
-  public MainView() {
+  private Toolbar myToolbar;
 
+  public MainView() {
+    myToolbar = new Toolbar(this);
+    this.setTop(myToolbar);
+
+
+
+  }
+
+  public void step() {
   }
 }

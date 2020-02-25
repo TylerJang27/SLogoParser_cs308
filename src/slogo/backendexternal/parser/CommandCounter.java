6a76516a0a239f68001119984e0499eb2a6b9d3b
Denharrs <dennis.harrsch@duke.edu>
2020-02-25 17:27:20 -0500

    Implemented basic command counter (still need to add more commands)

--- src/slogo/backendexternal/parser/CommandCounter.java
+++ src/slogo/backendexternal/parser/CommandCounter.java
@@ -4,5 +4,22 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class CommandCounter {
+  private Map<String, Integer> counts;
 
+  public CommandCounter(){
+    counts = new HashMap<>();
+    fillCounts();
+  }
+
+  public int getCount(String key){
+    return counts.get(key);
+  }
+
+  private void fillCounts(){
+    counts.put("Forward", 1);
+    counts.put("Home", 0);
+    counts.put("Backward", 1);
+    counts.put("Right", 1);
+    counts.put("ClearScreen", 1);
+  }
 }

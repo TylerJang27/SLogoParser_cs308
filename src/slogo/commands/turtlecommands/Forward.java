ae4b808518a0277347c8aead4e8105fc12ce3f36
wenyue gu <wg74@duke.edu>
2020-02-25 18:30:32 -0500

    changed coordinate in backward and forward

--- src/slogo/commands/turtlecommands/Forward.java
+++ src/slogo/commands/turtlecommands/Forward.java
@@ -39,7 +39,7 @@ public class Forward implements TurtleCommand {
         //  We'll see during testing
 
         double deltaX = returnVal*Math.sin(ret.get(ret.size()-1).getBearing());
-        double deltaY = -1 * returnVal*Math.cos(ret.get(ret.size()-1).getBearing()); //TODO: Why is this negative?
+        double deltaY = returnVal*Math.cos(ret.get(ret.size()-1).getBearing()); //TODO: Why is this negative?
         return TurtleCommand.move(ret.get(ret.size()-1), ret, deltaX, deltaY, xMax, yMax, mode);
     }
 

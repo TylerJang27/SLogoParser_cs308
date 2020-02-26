65b9d7f7f4a90574817ffed7ca607d001bfe434e
Tyler Jang <taj26@duke.edu>
2020-02-25 19:01:02 -0500

    resolved merge conflicts

--- src/slogo/backendexternal/TurtleModel.java
+++ src/slogo/backendexternal/TurtleModel.java
@@ -19,11 +19,6 @@ public class TurtleModel {
     //TODO: change implementation/display method/remove altogether?
     Collection<RunFunction> declaredFunctions;
 
-    public TurtleModel() {
-        declaredVariables = new ArrayList<>();
-        declaredFunctions = new ArrayList<>();
-    }
-
     public TurtleModel(List<Variable> vars, List<RunFunction> functions) {
         declaredVariables = vars;
         declaredFunctions = functions;

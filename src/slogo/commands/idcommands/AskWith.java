944782615cbb10c86c40bbdbb2875292bb398ac9
Tyler Jang <taj26@duke.edu>
2020-03-05 02:55:46 -0500

    implemented AskWith

--- src/slogo/commands/idcommands/AskWith.java
+++ src/slogo/commands/idcommands/AskWith.java
@@ -7,22 +7,24 @@ import slogo.commands.controlcommands.Constant;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 /**
- * Class that implements IdCommand, used to run a list of commands for specific turtles.
+ * Class that implements IdCommand, used to run a list of commands for turtles specified by conditions.
  *
  * @author Tyler Jang
  */
-public class Ask implements IdCommand {
+public class AskWith implements IdCommand {
     public static final int NUM_ARGS = 2;
     private Consumer<List<Integer>> idConsumer;
     private Supplier<Integer> singleIdSupplier;
     private Supplier<List<Integer>> idSupplier;
+    private Supplier<Map<Integer, TurtleStatus>> mapSupplier;
     private Consumer<Integer> idSetter;
     private Supplier<TurtleStatus> statusSupplier;
-    private List<Command> turtleIds;
+    private Command condition;
     private List<Command> commands;
 
     private double lastResult;
@@ -30,30 +32,29 @@ public class Ask implements IdCommand {
     /**
      * Constructor for Tell. Takes 2 command arguments.
      *
-     * @param ids Commands representing the different commands to set turtle IDs.
+     * @param cond Command representing the condition for turtles.
      * @param commandList Commands representing commands to run for specified IDs.
      * @param consumeIDs Consumer for modifying the activeTurtles in TurtleModel.
      * @param supplySingleID Supplier for retrieving the activeTurtle in TurtleModel.
      * @param supplyID Supplier for retrieving the activeTurtles in TurtleModel.
+     * @param supplyMap Supplier for retrieving information on all activeTurtles in TurtleModel.
      * @param consumeID Consumer for modifying the activeTurtle in TurtleModel.
      * @param supplyStat Supplier for retrieving the new activeTurtle Status in TurtleModel.
      */
-    public Ask(List<Command> ids, List<Command> commandList, Consumer<List<Integer>> consumeIDs, Supplier<Integer> supplySingleID, Supplier<List<Integer>> supplyID, Consumer<Integer> consumeID, Supplier<TurtleStatus> supplyStat) {
-        turtleIds = ids;
-        if (ids.isEmpty()) {
-            turtleIds = List.of(new Constant(1));
-        }
+    public AskWith(Command cond, List<Command> commandList, Consumer<List<Integer>> consumeIDs, Supplier<Integer> supplySingleID, Supplier<List<Integer>> supplyID, Supplier<Map<Integer, TurtleStatus>> supplyMap, Consumer<Integer> consumeID, Supplier<TurtleStatus> supplyStat) {
+        condition = cond;
         commands = commandList;
         idConsumer = consumeIDs;
         singleIdSupplier = supplySingleID;
         idSupplier = supplyID;
+        mapSupplier = supplyMap;
         idSetter = consumeID;
         statusSupplier = supplyStat;
     }
 
     //TODO: REFACTOR
     /**
-     * Executes the Ask instance, retrieving and setting the activeTurtles based on ids, running the commands, and resetting the active turtles.
+     * Executes the AskWith instance, determining ids to use, retrieving and setting the activeTurtles based on ids, running the commands, and resetting the active turtles.
      *
      * @param ts a singular TurtleStatus instance upon which to build subsequent TurtleStatus instances.
      *           TurtleStatus instances are given in absolutes, and thus may require other TurtleStatus values.
@@ -61,34 +62,26 @@ public class Ask implements IdCommand {
      */
     @Override
     public List<TurtleStatus> execute(TurtleStatus ts) {
-        List<Integer> previousIds = idSupplier.get();
-        Integer previousId = singleIdSupplier.get();
+        Map<Integer, TurtleStatus> allStatuses = mapSupplier.get();
         List<TurtleStatus> ret = new ArrayList<>();
 
-        List<Integer> ids = new ArrayList<>();
-        for (Command c: turtleIds) {
-            ids.add((int)Command.executeAndExtractValue(c, ts, ret));
-            ts = ret.get(ret.size() - 1);
+        List<Command> validIds = new ArrayList<>();
+
+        for (Map.Entry<Integer, TurtleStatus> e: allStatuses.entrySet()) {
+            ret.addAll(condition.execute(e.getValue()));
+            if (condition.returnValue()==1) {
+                validIds.add(new Constant(e.getKey()));
+            }
         }
 
-        idConsumer.accept(ids);
-        for (Integer id: ids) {
-            idSetter.accept(id);
-            ts = statusSupplier.get();
-            for (Command c: commands) {
-                lastResult = Command.executeAndExtractValue(c, ts, ret);
-                ts = ret.get(ret.size() - 1);
-            } //TODO: TEST FOR NESTED TELLS
-        }
-        idConsumer.accept(previousIds);
-        idSetter.accept(previousId);
-        ret.add(statusSupplier.get()); //TODO: ADJUST FOR DUPLICATION?
-
+        Ask asker = new Ask(validIds, commands, idConsumer, singleIdSupplier, idSupplier, idSetter, statusSupplier);
+        ret.addAll(asker.execute(ret.get(ret.size() - 1)));
+        lastResult = asker.returnValue();
         return ret;
     }
 
     /**
-     * Retrieves the value returned by Ask's execution, the last executed command's return value.
+     * Retrieves the value returned by AskWith's execution, the last executed command's return value.
      *
      * @return the last command's return value.
      */

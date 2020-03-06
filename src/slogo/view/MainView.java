2be10705bf9d251a5760749972e1a6cdfb1a8f11
Nevzat Sevim <ns217@duke.edu>
2020-03-05 23:10:05 -0500

    setPaneImageViews

--- src/slogo/view/MainView.java
+++ src/slogo/view/MainView.java
@@ -143,7 +143,8 @@ public class MainView extends VBox implements EventHandler, MainViewAPI {
   }
 
   public void setPaneImageViews() {
-
+    for(int i = 0; i < turtleManager.getImageViews().size(); i++)
+    pane.getChildren().set(i, turtleManager.getImageViews().get(i));
   }
 
   @Override

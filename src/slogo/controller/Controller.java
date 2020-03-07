4c13636d92a574fb71a657e2c53ce80fdc178197
Denharrs <dennis.harrsch@duke.edu>
2020-03-06 21:55:39 -0500

    Can now read in txt files as commands

--- src/slogo/controller/Controller.java
+++ src/slogo/controller/Controller.java
@@ -2,6 +2,9 @@ package slogo.controller;
 
 
 import java.io.File;
+import java.io.FileReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -51,7 +54,9 @@ public class Controller extends Application {
   /**
    * Start of the program.
    */
-  public static void main(String[] args) { launch(args); }
+  public static void main(String[] args) {
+    launch(args);
+  }
 
   @Override
   public void start(Stage currentStage) {
@@ -78,13 +83,13 @@ public class Controller extends Application {
 
   private void setTabs() {
     tabs = myDisplay.getTabPane().getTabs();
-    for(Tab tab : tabs){
+    for (Tab tab : tabs) {
       tab.setOnSelectionChanged(event -> setListeners(tab));
     }
   }
 
-  private void changeOnWrite(){
-    for(Tab tab : tabs){
+  private void changeOnWrite() {
+    for (Tab tab : tabs) {
       MainView tabMainView = (MainView) tab.getGraphic();
       TextField tabConsole = tabMainView.getTextFields().getConsole().getEntry();
       tabConsole.setOnMouseClicked(event -> setListeners(tab));
@@ -111,13 +116,13 @@ public class Controller extends Application {
     runButton = mainView.getToolBar().getCommandButton();
     runButton.setOnAction(event -> sendCommand());
     uploadFile = mainView.getToolBar().getUploadFile();
-    uploadFile.setOnAction( event -> chooseFile());
+    uploadFile.setOnAction(event -> chooseFile());
     language = mainView.getToolBar().getLanguageBox();
     language.setOnAction(event -> setLanguage(language));
     modeMenu = mainView.getToolBar().getModeMenu();
     modeMenu.setOnAction(event -> setMode(modeMenu));
     arrows = mainView.getTextFields().getMoveArrows();
-    for(Button arrow : arrows.getButtons()){
+    for (Button arrow : arrows.getButtons()) {
       arrow.setOnAction(event -> moveTurtle(arrow, arrows.getIncrement()));
     }
   }
@@ -128,12 +133,12 @@ public class Controller extends Application {
   }
 
 
-  private void sendCommand(){
-    try{
+  private void sendCommand() {
+    try {
       myParser.parseLine(console.getText());
       List<Command> toSend = myParser.sendCommands();
       List<TurtleStatus> statuses = myModel.executeCommands(toSend);
-      if(statuses.size() > 0){
+      if (statuses.size() > 0) {
         setStatus(statuses.get(statuses.size() - 1));
         myDisplay.getMainView().moveTurtle(statuses);
       }
@@ -141,41 +146,39 @@ public class Controller extends Application {
       console.displayHistory();
       displayVariables();
       displayQueries();
-    }
-    catch(Exception e){
+    } catch (Exception e) {
       console.addError(errorHandler.getErrorMessage(e.getMessage(), myParser.getCommands()));
       console.getEntry().setOnKeyPressed(key -> handlePrompt(key.getCode()));
     }
   }
 
-  private void handlePrompt(KeyCode key){
-    if(key == KeyCode.Y){
+  private void handlePrompt(KeyCode key) {
+    if (key == KeyCode.Y) {
       console.getEntry().setText(errorHandler.fixLine(myParser.getLastLine()));
       sendCommand();
     }
-    if(key == KeyCode.N){
+    if (key == KeyCode.N) {
       console.displayHistory();
       displayVariables();
       displayQueries();
     }
   }
 
-  private void setStatus(TurtleStatus ts){
+  private void setStatus(TurtleStatus ts) {
     currentStatus = ts;
   }
 
-  private void displayVariables(){
+  private void displayVariables() {
     myDisplay.getMainView().getTextFields().clearVariables();
-    List<String> variables =  myParser.getVariableString();
+    List<String> variables = myParser.getVariableString();
     List<String> functions = myParser.getFunctionString();
-    if(variables.size() + functions.size() <= 0) {
+    if (variables.size() + functions.size() <= 0) {
       myDisplay.getMainView().getTextFields().addVariableText("");
-    }
-    else{
-      for(String variable : myParser.getVariableString()){
+    } else {
+      for (String variable : myParser.getVariableString()) {
         myDisplay.getMainView().getTextFields().addVariableText(variable);
       }
-      for(String function : myParser.getFunctionString()){
+      for (String function : myParser.getFunctionString()) {
         myDisplay.getMainView().getTextFields().addVariableText(function);
       }
     }
@@ -187,7 +190,7 @@ public class Controller extends Application {
     myDisplay.getMainView().getTextFields().addQueriesText();
   }
 
-  private void setLanguage(ComboBox language){
+  private void setLanguage(ComboBox language) {
     String newLanguage = language.getValue().toString();
     console.translateHistory(translator, newLanguage);
     userDefinitions.translateDefinitions(translator, newLanguage);
@@ -195,7 +198,7 @@ public class Controller extends Application {
     myParser.setLanguage(translator);
   }
 
-  private void setMode(ComboBox menu){
+  private void setMode(ComboBox menu) {
     myParser.setMode(menu.getValue().toString());
   }
 
@@ -203,8 +206,24 @@ public class Controller extends Application {
     FileChooser fileChooser = new FileChooser();
     Stage fileStage = new Stage();
     File file = fileChooser.showOpenDialog(fileStage);
-    if(file != null){
-     //read file
+    try {
+      readFile(file);
+    } catch (Exception e) {
+      console.setText("File could not be read");
+    }
+  }
+
+  private void readFile(File file) {
+    try {
+      String contents = new String(Files.readAllBytes(Paths.get(file.getPath())));
+      StringBuilder builder = new StringBuilder();
+      for (String s : contents.split("\n")) {
+        builder.append(s);
+        builder.append(" ");
+      }
+      console.setText(builder.toString());
+    } catch (Exception e) {
+      console.setText("File could not be read");
     }
   }
 }

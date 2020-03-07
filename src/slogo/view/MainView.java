95abc6f03ebc20135de9d877b1664e99cce22129
Shruthi Kumar <sk593@duke.edu>
2020-03-07 01:54:37 -0500

    added comments to mainview

--- src/slogo/view/MainView.java
+++ src/slogo/view/MainView.java
@@ -22,7 +22,14 @@ import slogo.frontendexternal.TurtleView;
 import slogo.frontendexternal.TurtleViewManager;
 import slogo.view.InputFields.InputFields;
 
-/** @author Shruthi Kumar, Nevzat Sevim */
+
+/**
+ * This class sets up the MainView that will host the toolbar, the pane, and the input fields (console, list of variables, etc)
+ * Purpose: This initializes and updates the display. Users can input commands and see their commands play out on the pane as the turtle moves.
+ * Assumptions: The class will work assuming all dependencies on Toolbar and the InputFields are working
+ * Dependencies: This class relies on the Toolbar, Display, and InputFields, as well as resources files like the images
+ * @author Shruthi Kumar, Nevzat Sevim
+ **/
 
 public class MainView extends VBox implements EventHandler {
   public static final double SCREEN_WIDTH = (int) Screen.getPrimary().getBounds().getWidth() - 300;
@@ -44,15 +51,14 @@ public class MainView extends VBox implements EventHandler {
   private String defaultTurtleFileName;
   private String defaultCodeFileName;
 
-  private final double turtleSize = 90;
-  private Insets insets = new Insets(0);
-
   private TurtleView turtle;
   private TurtleViewManager turtleManager;
   private TurtleStatus turtleStatus;
 
 
-
+  /**
+   * Constructor for the MainView object
+   */
   public MainView() {
     // Get the Textfield and Toolbar in the MainView
     setUpMainViewFields(Color.LIGHTGRAY, Color.BLACK, 1, "raphael", "blank");
@@ -68,7 +74,14 @@ public class MainView extends VBox implements EventHandler {
     this.setAlignment(Pos.TOP_LEFT);
   }
 
-
+  /**
+   * Overloaded constructor used when preferences are loaded from a file
+   * @param backgroundColor : display background color
+   * @param penColor : pen color
+   * @param numTurtle : number of turtles
+   * @param turtleImageName : image name of turtle
+   * @param codeFileName : name of code file
+   */
   public MainView(Color backgroundColor, Color penColor, int numTurtle, String turtleImageName, String codeFileName) {
     // Get the Textfield and Toolbar in the MainView
     setUpMainViewFields(backgroundColor, penColor, numTurtle, turtleImageName, codeFileName);
@@ -84,7 +97,10 @@ public class MainView extends VBox implements EventHandler {
     this.turtleStatus = new TurtleStatus(1);
   }
 
-
+  /**
+   * Moves the turtle based on given statuses that execute commands
+   * @param ts: list of turtle statuses
+   */
   public void moveTurtle(List<TurtleStatus> ts) {
     if (!ts.isEmpty()) {
       turtleManager.execute(ts);
@@ -94,6 +110,9 @@ public class MainView extends VBox implements EventHandler {
     }
   }
 
+  /**
+   * Updates location of the image view of the turtle
+   */
   public void updateViewLocation() {
     pane.getChildren().clear(); // clear complete list
     pane.getChildren().addAll(turtleManager.getImageViews());
@@ -107,71 +126,137 @@ public class MainView extends VBox implements EventHandler {
     }
   }
 
+  /**
+   * Gets the TurtleView
+   * @return turtle: turtleView
+   */
   public TurtleView getTurtle() {
     return turtle;
   }
 
+  /**
+   * Gets the turtle manager
+   * @return turtleManager : turtle view manager
+   */
   public TurtleViewManager getTurtles() {
     return turtleManager;
   }
 
+  /**
+   * Sets the layout of all image views
+   */
   public void setImageViewLayouts() {
     for (TurtleView tv: turtleManager.getTurtleViews()) {
       tv.setUpMyImageView();
     }
   }
 
+  /**
+   * Sets the image views on the pane
+   */
   public void setPaneImageViews() {
     for(int i = 0; i < turtleManager.getImageViews().size(); i++) {
       pane.getChildren().set(i, turtleManager.getImageViews().get(i));
     }
   }
 
+  /**
+   * sets the background color
+   * @param newColor : new background color
+   */
   public void setBackgroundColor(Color newColor) {
     this.pane.setBackground(new Background(new BackgroundFill(newColor, CornerRadii.EMPTY, new Insets(0))));
     defaultBackgroundColor = newColor;
   }
 
+  /**
+   * Sets the new pen color
+   * @param newColor : new pen color
+   */
   public void setPenColor(Color newColor) {
     this.turtleManager.setAllPenViewColors(newColor);
     defaultPenColor = newColor;
   }
 
+  /**
+   * sets the name of the image file
+   * @param name : turtle image file name
+   */
   public void setTurtleFileName(String name) {
     defaultTurtleFileName = name;
   }
 
 
+  /**
+   * Overriden method that handles events
+   * @param event : event
+   */
   @Override
   public void handle(Event event) { }
 
+  /**
+   * Gets the default background color
+   * @return defaultBackgroundColor : default background color
+   */
   public Color getDefaultBackgroundColor() {
     return defaultBackgroundColor;
   }
 
+  /**
+   * Gets the default pen color
+   * @return defaultPenColor : default pen color
+   */
   public Color getDefaultPenColor() {
     return defaultPenColor;
   }
 
+  /**
+   * Gets default number of turtles
+   * @return number of turtles
+   */
   public int getDefaultNumTurtles() {
     return turtleManager.getTurtleViews().size();
   }
 
+  /**
+   * Gets turtle image name
+   * @return defaultTurtleFileName : turtle file name
+   */
   public String getDefaultTurtleFileName() {
     return defaultTurtleFileName;
   }
 
+  /**
+   * Gets default code file name
+   * @return defaultCodeFileName : code file name
+   */
   public String getDefaultCodeFileName() {
     return defaultCodeFileName;
   }
 
   //Public Get Methods
+
+  /**
+   * Gets the input fields
+   * @return myInpurFields : input fields
+   */
   public InputFields getTextFields(){return this.myInputFields;}
+
+  /**
+   * Gets the tool bar
+   * @return myToolbar : tool bar
+   */
   public Toolbar getToolBar(){return this.myToolbar;}
   //public Pane getPane() {return this.pane;}
   //public double getTurtleSize() {return this.turtleSize;}
+
+  /**
+   * Gets the turtle status
+   * @return turtleStatus : turtle status
+   */
   public TurtleStatus getTurtleStatus() {return this.turtleStatus;}
 
+  
   private void setUpMainViewFields(Color backgroundColor, Color penColor, int numTurtles, String turtleImageName,
       String codeFileName) {
     this.myInputFields = new InputFields(this);
